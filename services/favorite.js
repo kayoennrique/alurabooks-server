f3cd5d09f5cd8b366d4e3b28896955066b4c374d
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-29 20:25:58 -0300

    Favorite Books Service

---
+++ services/favorite.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+function getAllFavorites() {
+  return JSON.parse(fs.readFileSync("favorites.json"));
+}
+
+function deleteFavoriteById(id) {
+  const books = JSON.parse(fs.readFileSync("favorites.json"));
+
+  const booksFiltered = books.filter(book => book.id !== id);
+  fs.writeFileSync("favorites.json", JSON.stringify(booksFiltered));
+}
+
+function insertFavorite(id) {
+  const books = JSON.parse(fs.readFileSync("books.json"));
+  const favorites = JSON.parse(fs.readFileSync("favorites.json"));
+
+  const bookInserted = books.find(book => book.id === id);
+  const newBookListFavorites = [...favorites, bookInserted];
+  fs.writeFileSync("favorites.json", JSON.stringify(newBookListFavorites));
+}
+
+module.exports = {
+  getAllFavorites,
+  deleteFavoriteById,
+  insertFavorite
+};
