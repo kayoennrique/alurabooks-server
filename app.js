04a79d364dd8435caee3c67eb78f5b96378964c4
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-29 20:12:21 -0300

    Installing cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bookRoute = require('./routes/book');
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({ origin: "*" }))
 
 app.use('/books', bookRoute);
 
